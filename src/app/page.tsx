7f6bcc893a8c706ecf202ffa44e576ea29c02aba
Chuseok22 <bjh59629@naver.com>
2025-10-01 11:00:31 +0900

    홈_화면_UI_개발 : feat : ActionCard 컴포넌트 분리 https://github.com/Chuseok22/time-mate-web/issues/3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { CalendarPlus, Users } from "lucide-react";
-import Link from "next/link";
+import ActionCard from "@/features/home/components/ActionCard";
 
 export default function Home() {
   return (
@@ -22,51 +22,26 @@ export default function Home() {
         </div>
 
         <div className="flex flex-col w-full gap-8">
-          <div className="flex flex-col bg-white w-full items-center rounded-2xl py-10 gap-6">
-            <div className="flex flex-row w-10/12 items-center justify-center gap-3">
-              <div className="flex flex-1 items-center justify-center">
-                <div className="bg-main p-3.5 rounded-2xl">
-                  <CalendarPlus />
-                </div>
-              </div>
-              <div className="flex flex-col flex-5 gap-1">
-                <div className="text-xl font-bold">
-                  새 모임 만들기
-                </div>
-                <div>
-                  새로운 모임을 만들어 보세요
-                </div>
-              </div>
-            </div>
-            <Link
-                href="/create-meeting"
-                className="bg-blue-500 text-white flex w-10/12 justify-center font-bold rounded-2xl py-3.5 hover:cursor-pointer hover:bg-blue-300 transition">
-              모임 만들기
-            </Link>
-          </div>
-
-          <div className="flex flex-col bg-white w-full items-center rounded-2xl py-10 gap-6">
-            <div className="flex flex-row w-10/12 items-center justify-center gap-3">
-              <div className="flex flex-1 items-center justify-center">
-                <div className="bg-green-100 p-3.5 rounded-2xl">
-                  <Users />
-                </div>
-              </div>
-              <div className="flex flex-col flex-5 gap-1">
-                <div className="text-xl font-bold">
-                  모임 참여하기
-                </div>
-                <div>
-                  초대받은 모임에 참여해보세요
-                </div>
-              </div>
-            </div>
-            <Link
-                href="/join-meeting"
-                className="bg-green-500 text-white flex w-10/12 justify-center font-bold rounded-2xl py-3.5 hover:cursor-pointer hover:bg-green-300 transition">
-              모임 만들기
-            </Link>
-          </div>
+          <ActionCard
+              title="새 모임 만들기"
+              description="새로운 모임을 만들어 보세요"
+              icon={CalendarPlus}
+              iconBgColor="bg-main"
+              buttonText="모임 만들기"
+              buttonColor="bg-blue-500"
+              buttonHoverColor="hover:bg-blue-300"
+              href="/craete-meeting"
+          />
+          <ActionCard
+              title="모임 참여하기"
+              description="초대받은 모임에 참여해보세요"
+              icon={Users}
+              iconBgColor="bg-green-100"
+              buttonText="모임 참여하기"
+              buttonColor="bg-green-500"
+              buttonHoverColor="hover:bg-green-300"
+              href="/join-meeting"
+          />
         </div>
 
 
---
+++ src/features/home/components/ActionCard.tsx
@@ -0,0 +1,49 @@
+import { LucideIcon } from "lucide-react";
+import Link from "next/link";
+
+interface ActionCardProps {
+  title: string;
+  description: string;
+  icon: LucideIcon;
+  iconBgColor: string;
+  buttonText: string;
+  buttonColor: string;
+  buttonHoverColor: string;
+  href: string;
+}
+
+export default function ActionCard({
+  title,
+  description,
+  icon: Icon,
+  iconBgColor,
+  buttonText,
+  buttonColor,
+  buttonHoverColor,
+  href,
+}: ActionCardProps) {
+  return (
+      <div className="flex flex-col bg-white w-full items-center rounded-2xl py-10 gap-6">
+        <div className="flex flex-row w-10/12 items-center justify-center gap-3">
+          <div className="flex flex-1 items-center justify-center">
+            <div className={`${iconBgColor} p-3.5 rounded-2xl`}>
+              <Icon />
+            </div>
+          </div>
+          <div className="flex flex-col flex-5 gap-1">
+            <div className="text-xl font-bold">
+              {title}
+            </div>
+            <div>
+              {description}
+            </div>
+          </div>
+        </div>
+        <Link
+            href={href}
+            className={`${buttonColor} text-white flex w-10/12 justify-center font-bold rounded-2xl py-3.5 hover:cursor-pointer ${buttonHoverColor} transition`}>
+          {buttonText}
+        </Link>
+      </div>
+  )
+};
