00aae63e0535d9df7eb8477e1a0f07493f4c80fb
Chuseok22 <bjh59629@naver.com>
2025-10-14 09:04:42 +0900

    모임_상세_페이지_개발 : feat : 환경 변수 미등록 시 오류 발생 분기처리 https://github.com/Chuseok22/time-mate-web/issues/13

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,4 +1,12 @@
 const ORIGIN_URL = process.env.ORIGIN_URL
 const API_BASE_URL = process.env.API_BASE_URL
 
-export { ORIGIN_URL, API_BASE_URL }
+if (!ORIGIN_URL) {
+  throw new Error('ORIGIN_URL 환경 변수가 설정되지 않았습니다.');
+}
+
+if (!API_BASE_URL) {
+  throw new Error('API_BASE_URL 환경 변수가 설정되지 않았습니다.');
+}
+
+export { ORIGIN_URL, API_BASE_URL };
